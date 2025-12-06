8483233254fc1586b7d9fa97f3c7a0c6e940cd7f
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2025-12-05 22:12:02 -0800

    storage engine

--- app.js
+++ app.js
@@ -9,6 +9,28 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'file_storage/');
+    },
+    filename: (req, file, cb) => {
+        const file_name = `${Date.now()}-${file.originalname}`;
+        cb(null, file_name);
+    }
+});
+
+const upload = multer({storage});
+
+app.use('/uploads', express.static(path.join(__dirname, 'file_storage')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.redirect('/')
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
